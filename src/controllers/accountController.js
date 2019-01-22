accfc6dd431343cad823a2919f97f9ed0e14ee61
Lee-HL <2585108306@qq.com>
2019-01-22 10:49:51 +0800

    完成了新增学生信息功能

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,12 +1,6 @@
 const path = require('path')
-const MongoClient = require('mongodb').MongoClient
+const databasetool = require(path.join(__dirname, "../tools/databasetool.js"));
 const captchapng = require('captchapng')
-
-// Connection URL
-const url = 'mongodb://localhost:27017';
- 
-// Database Name
-const dbName = 'szhmqd27_lhl';
 
 /**
  * module.exports = {
@@ -32,42 +26,27 @@ exports.register = (req,res) => {
     
     // 2.先判断数据中用户名,是否存在,如果存在返回提示
     // Use connect method to connect to the server
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-        // 拿到db
-        const db = client.db(dbName);
-
-        // 拿到集合
-        const collection = db.collection('accountInfo');
-
-        // 查询一个
-        collection.findOne({username},(err,doc)=>{
-            // 如果doc == null 没有查询到,就可以插入,如果查询到了,说明用户名已经存在
-            if (doc) {// 存在
-                result.status = 1
-                result.message = "用户名已经存在"
-
-                // 关闭数据困
-                client.close();
-
-                // 返回
-                res.json(result)
-            }else {
-                // 3. 如果用户名不存在,插入数据库中
-                // result2 有值, 代表成功 result2 为null就是失败
-                collection.insertOne(req.body,(err,result2)=>{
-                    if (!result2) {
-                        result.status = 2
-                        result.message = "注册失败"
-                    }
-
-                    // 关闭数据库
-                    client.close();
-
-                    // 返回
-                    res.json(result);
-                })
-            }   
-        })
+    databasetool.findYige("accountInfo", { username }, (err, doc) => {
+        // 如果result == null 没有查询到，就可以插入，如果查询到了，说明用户名已经存在
+        if (doc) {
+            // 存在
+            result.status = 1;
+            result.message = "用户名已经存在";
+    
+            // 返回
+            res.json(result);
+        } else {
+            databasetool.insertSingle("accountInfo", req.body, (err, result2) => {
+            if (!result2) {
+              // 失败
+                result.status = 2;
+                result.message = "注册失败";
+            }
+    
+            // 返回
+            res.json(result);
+            });
+        }
     });
 }
 
@@ -110,28 +89,19 @@ exports.login = (req,res) => {
         result.message = "验证码错误"
 
         res.json(result)
-        // return
+        return
     }
 
     // 验证码正确了
-     // Use connect method to connect to the server
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-        // 拿到db对象
-        const db = client.db(dbName);
-
-        // 拿到集合
-        const collection = db.collection('accountInfo');
-
-        //
-        collection.findOne({username,password},(err,doc)=>{
-            if (!doc) {
-                result.status = 2
-                result.message = "用户名或是密码错误"
-            }
-            
-            // 关闭数据库
-            client.close();
-            res.json(result)
-        })
-    })
+    // Use connect method to connect to the server
+    databasetool.findYige("accountInfo", { username, password }, (err, doc) => {
+        // 这里的代码，要等到databasetool中的callback执行的时候，才会执行
+        // 那边会把 err和doc传递过来
+        if (!doc) {
+            result.status = 2;
+            result.message = "用户名或是密码错误";
+        }
+    
+        res.json(result);
+    });
 }
